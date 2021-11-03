089f736b880a985efe3f043ca252240352f92581
Leonardo Lopes <leonardolopesb97@hotmail.com>
2021-11-03 10:33:29 -0300

    feat: :heavy_plus_sign: Importando arquivos CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const isDevelopment = process.env.NODE_ENV !== 'production';
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  mode: isDevelopment ? "development" : 'production',
-  devtool: isDevelopment ? 'eval-source-map': 'source-map',
+  mode: isDevelopment ? "development" : "production",
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
   entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -29,6 +29,11 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
